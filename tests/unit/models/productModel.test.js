a8a4f4f7e57b8d00a959051cde37c61dc1a0e8ed
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-29 16:15:32 -0300

    test: Finalizado tests para getById model
    alterado alguns comportamentos esperados para melhor tratamento nas demais camadas

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -5,7 +5,7 @@ const productModel = require('../../../models/productModel');
 const connection = require('../../../models/connection');
 
 // reaproveitando mock criado pela trybe
-const { allProductsResponse } = require('../../../__tests__/_dataMock');
+const { allProductsResponse, productSearchNameResponse } = require('../../../__tests__/_dataMock');
 
 // Apenas valores simbólicos devido ao stub, esse valores não tem importância.
 const NOT_FOUND_ID = 999;
@@ -30,7 +30,7 @@ describe('Testing productModel', () => {
         stub(connection, 'query').resolves([allProductsResponse]);
       });
       after(() => connection.query.restore());
-
+      
       it('Should return an array of objects', async () => {
         const result = await productModel.getAll();
         expect(result).not.to.be.empty;
@@ -44,20 +44,32 @@ describe('Testing productModel', () => {
   })
   describe('Getting products by id', () => {
     describe('When the id is not found', () => {
+      before(async () => {
+        stub(connection, 'query').resolves([[]]);
+      });
+      after(() => connection.query.restore());
+
       it('Should return an empty array', async () => {
-        const [result] = await productModel.getById(NOT_FOUND_ID);
+        const result = await productModel.getById(NOT_FOUND_ID);
         expect(result).to.be.an('array');
         expect(result).to.be.empty;
       })
     });
     describe('When the id is found', () => {
-      it('Should return an object', async () => {
-        const result = await productModel.getById(FOUND_ID);
-        expect(result).to.be.an('object');
+      before(async () => { 
+        stub(connection, 'query').resolves([productSearchNameResponse]);
       });
-      it('Should have the keys "id" and "name"', async () => {
+      after(() => connection.query.restore());
+      
+      it('Should return an array of objects', async () => {
         const result = await productModel.getById(FOUND_ID);
-        expect(result).to.includes.all.keys('id', 'name');
+        expect(result).to.be.an('array');
+        expect(result[0]).to.be.an('object');
+      });
+      it('Should have the keys "id" with the value "1" and "name" with the value "Martelo de Thor" ', async () => {
+        const [result] = await productModel.getById(FOUND_ID);
+        expect(result.id).to.be.equal(1);
+        expect(result.name).to.be.equal('Martelo de Thor');
       })
     });
   })
