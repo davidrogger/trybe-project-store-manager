c01a1c7efbc563ab5ad447e3f6513fe739a36e5c
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-29 15:55:43 -0300

    test: Finalizado tests para getAll model
    adicionado importação de funções e bibliotecas necessárias para finalizar testes relacionados ao getAll

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -1,10 +1,11 @@
 const { expect } = require('chai');
+const { stub } = require('sinon');
 
-// dummy TDD
-const productModel = {
-  getAll: () => { },
-  getById: () => { },
-};
+const productModel = require('../../../models/productModel');
+const connection = require('../../../models/connection');
+
+// reaproveitando mock criado pela trybe
+const { allProductsResponse } = require('../../../__tests__/_dataMock');
 
 // Apenas valores simbólicos devido ao stub, esse valores não tem importância.
 const NOT_FOUND_ID = 999;
@@ -13,21 +14,31 @@ const FOUND_ID = 1;
 describe('Testing productModel', () => {
   describe('Getting all products', () => {
     describe('When there is no product into the database', () => {
+      before(async () => {
+        stub(connection, 'query').resolves([[]]);
+      });
+      after(() => connection.query.restore());
+
       it('Should return an empty array', async () => {
-        const [result] = await productModel.getAll();
+        const result = await productModel.getAll();
         expect(result).to.be.an('array');
         expect(result).to.be.empty;
       });
     });
     describe('When there is products into the database', () => {
+      before(async () => {
+        stub(connection, 'query').resolves([allProductsResponse]);
+      });
+      after(() => connection.query.restore());
+
       it('Should return an array of objects', async () => {
-        const [result] = await productModel.getAll();
+        const result = await productModel.getAll();
         expect(result).not.to.be.empty;
         expect(result[0]).to.be.an('object');
       });
       it('Should have in each object the keys "id" and "name"', async () => {
-        const [result] = await productModel.getAll();
-        result.forEach((item) => expect(item).to.includes.keys('id', 'name'));
+        const result = await productModel.getAll();
+        result.forEach((item) => expect(item).to.includes.all.keys('id', 'name'));
       });
     })
   })
@@ -46,7 +57,7 @@ describe('Testing productModel', () => {
       });
       it('Should have the keys "id" and "name"', async () => {
         const result = await productModel.getById(FOUND_ID);
-        expect(result).to.includes.keys('id', 'name');
+        expect(result).to.includes.all.keys('id', 'name');
       })
     });
   })
