519886184268eac40a388016a934dae050689dac
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-30 17:03:57 -0300

    feat: Rota responsavel pela remoção do produto
    adicionado rota que chama o controller para iniciar os processos de remoção do item solicitado

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -6,6 +6,7 @@ const productRouter = Router();
 
 productRouter.get('/:id', rescue(productController.getById));
 productRouter.put('/:id', rescue(productController.update));
+productRouter.delete('/:id', rescue(productController.remove));
 productRouter.get('/', rescue(productController.getAll));
 productRouter.post('/', rescue(productController.add));
 
