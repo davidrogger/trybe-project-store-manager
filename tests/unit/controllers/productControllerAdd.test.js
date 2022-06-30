429d5edf80b5167b427915f45005afd7a22c0d7e
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-30 14:51:15 -0300

    test: Esrutura inicial productController add
    Teste inicial sem funcionalidade implementada

---
+++ tests/unit/controllers/productControllerAdd.test.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const { stub } = require('sinon');
+
+const { productCreateResponse, rightProductBody } = require('../../../__tests__/_dataMock');
+
+const status = require('../../../helpers/status');
+
+const productController = require('../../../controllers/productController');
+const productService = require('../../../services/productService');
+
+describe('Testing productControler ADD', () => {
+  const request = {};
+  const response = {};
+  describe('When the body is correct', () => {
+    before(async () => {
+      request.body = rightProductBody;
+      response.status = stub().returns(response);
+      response.json = stub().returns();
+
+      stub(productService, 'validateProductBody').resolves(rightProductBody)
+      stub(productService, 'add').resolves(productCreateResponse)
+    })
+    it('Should response 201 with a json "id" 4 and name "Produto1"', async () => {
+      await productController.add(request, response);
+      expect(response.status.calledWith(status.HTTP_OK_REQUEST).to.be.equal(true));
+      expect(response.json.calledWith(productCreateResponse)).to.be.equal(true);
+
+    });
+  });
+});
