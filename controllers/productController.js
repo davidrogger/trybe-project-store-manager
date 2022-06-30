27820daf5cc67f4a208a950efa38be3e56ce7f0d
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-30 10:25:35 -0300

    fix: adicionado espera para validar o id
    Foi necessaria adicionar espera para validar o id

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,7 +7,7 @@ const productController = {
     res.status(status.HTTP_OK_REQUEST).json(result);
   },
   async getById(req, res) {
-    const { id } = productService.validateId(req.params);
+    const { id } = await productService.validateId(req.params);
     const result = await productService.getById(id);
     res.status(status.HTTP_OK_REQUEST).json(result);
   },
