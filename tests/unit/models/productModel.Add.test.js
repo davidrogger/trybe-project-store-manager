72f8af4c2e7f35c572329482cd83030c8f530950
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-30 11:54:48 -0300

    feat: adicionado mock de product
    adicionado objecto com nome do produto adicionado

--- tests/unit/models/productModel.Add.test.js
+++ tests/unit/models/productModel.Add.test.js
@@ -3,16 +3,18 @@ const { stub } = require('sinon');
 const connection = require('../../../models/connection');
 const productModel = require('../../../models/productModel');
 
+const { rightProductBody } = require('../../../__tests__/_dataMock');
+
 describe('Testing productModel ADD', () => {
   before(async () => {
-    stub(connection, 'query').resolves({ id: 10 });
+    stub(connection, 'query').resolves([{ insertId: 10 }]);
   });
   after(() => {
     connection.query.restore();
   })
 
   it('Should return an object with the id of the product created', async () => {
-    const result = await productModel.add();
+    const result = await productModel.add(rightProductBody);
     expect(result).to.deep.equal({ id: 10 });
   });
 });
