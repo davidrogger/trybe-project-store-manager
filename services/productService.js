d0fdd464b679d30a146f6f36ca7066551bd0d476
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-30 09:01:31 -0300

    feat: criada função validateId
    Responsavel por verificar se o Id fornecido é valido para o funcionamento da busca

--- services/productService.js
+++ services/productService.js
@@ -1,15 +1,19 @@
+const Joi = require('joi');
 const productModel = require('../models/productModel');
-
+const { runSchema } = require('./validator');
 const { NotFoundError } = require('../helpers/NotFoundError');
 
 const productService = {
+  validateId: runSchema(Joi.object({
+    id: Joi.number().required().positive().integer(),
+  })),
   async getAll() {
     const result = await productModel.getAll();
     return result;
   },
   async getById(id) {
     const result = await productModel.getById(id);
-    if (!result) throw new NotFoundError('Product not found');
+    if (result.length === 0) throw new NotFoundError('Product not found');
     return result[0];
   },
 };
