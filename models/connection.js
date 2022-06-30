252a18c4e4e31e68a40a1c8fe62251159d30607c
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-30 10:26:25 -0300

    feat: removação de porta
    porta estava incorreta gerando problemas.

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: process.env.PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
 });
