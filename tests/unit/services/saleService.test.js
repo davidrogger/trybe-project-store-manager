205bf108406d37125e5131a66fd1ef979f86dd77
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-01 15:43:15 -0300

    test: saleService getAll
    Adicionando stub para o teste ficar isolado

--- tests/unit/services/saleService.test.js
+++ tests/unit/services/saleService.test.js
@@ -1,10 +1,17 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
+const { stub } = require('sinon');
 const saleService = require("../../../services/saleService");
+const saleModel = require("../../../models/saleModel");
 
 describe.only('Testing sale Service GET', () => {
   describe('Getting all sales', () => {
+    before(async () => {
+      stub(saleModel, 'getAll').resolves([{}]);
+    });
+
     it('Should return an array of objects and call the model responsable to get into the database', async () => {
       const result = await saleService.getAll();
+      expect(saleModel.getAll.calledOnce).to.be.equal(true);
       expect(result).to.be.an('array');
       expect(result[0]).to.be.an('object');
 
