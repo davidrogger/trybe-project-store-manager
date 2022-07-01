0946560813ae140c464307204eb9661474e13f25
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-01 16:05:39 -0300

    test: saleController getAll
    Ajuste de posição para stub

--- tests/unit/controllers/saleControllerGet.test.js
+++ tests/unit/controllers/saleControllerGet.test.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const { stub } = require('sinon');
 const saleController = require('../../../controllers/saleController');
 const saleService = require('../../../services/saleService');
 const status = require('../../../helpers/status');
@@ -9,17 +10,17 @@ describe.only('Testing saleController GET', () => {
   const request = {};
   const response = {};
 
+  before(async () => { 
+    response.status = stub().returns(response);
+    response.json = stub().returns();
+
+    stub(saleService, 'getAll').resolves(salesStubData);
+  });
+  after(() => {
+    saleService.getAll.restore();
+  });
+
   describe('Getting all sales', () => { 
-    before(async () => { 
-      response.status = stub().returns(response);
-      response.json = stub().returns();
-
-      stub(saleService, 'getAll').resolves(salesStubData);
-    });
-    after(() => {
-      saleService.getAll.restore();
-    });
-
     it('Should response 200 and a json with all sales', async () => {
       await saleController.getAll(request, response);
       expect(response.status.calledWith(status.HTTP_OK_REQUEST)).to.be.equal(true);
