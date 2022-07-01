3eb6be2a8162a0516e6343a1d2ddd744a99cec86
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-01 15:11:49 -0300

    feat: saleModel Get
    Implementada funcionalidade que coleta todas vendas do banco de dados.

--- models/saleModel.js
+++ models/saleModel.js
@@ -1,6 +1,18 @@
+const connection = require('./connection');
+
 const saleModel = {
   async getAll() {
-    //
+    const query = `
+    SELECT
+      sales_products.sale_id AS saleId,
+      sales.date, products.id AS productId,
+      sales_products.quantity
+    FROM StoreManager.sales_products
+    INNER JOIN StoreManager.sales ON sales.id = sales_products.sale_id
+    INNER JOIN StoreManager.products ON sales_products.product_id = products.id;
+    `;
+    const [result] = await connection.query(query);
+    return result;
   },
 };
 
