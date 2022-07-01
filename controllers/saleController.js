866798239a0f2e7c1b06bf38e6f5d7976821a37b
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-01 16:04:57 -0300

    feat: saleController geAll
    Implementada funcionalidade que chama o service para coletar as vendas

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -1,6 +1,10 @@
+const status = require('../helpers/status');
+const saleService = require('../services/saleService');
+
 const saleController = {
-  async getAll() { 
-    //
+  async getAll(_req, res) { 
+    const result = await saleService.getAll();
+    res.status(status.HTTP_OK_REQUEST).json(result);
   },
 };
 
