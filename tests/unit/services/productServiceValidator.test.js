f3c46ad56dbf3d7c00b468e004f67f241e132d5b
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-01 10:03:31 -0300

    test: productService Validator
    Testando quando o id é invalido e valido

--- tests/unit/services/productServiceValidator.test.js
+++ tests/unit/services/productServiceValidator.test.js
@@ -1,37 +1,53 @@
 const { expect } = require('chai');
 const { stub } = require('sinon');
+const Joi = require('joi');
 const productService = require('../../../services/productService');
-const productModel = require('../../../models/productModel');
+
+const schema = Joi.object();
 
 describe('Testing productService Validator', () => {
   afterEach(() => {
-    productModel.update.restore();
+      schema.validate.restore();
   });
   describe('Validating an "id"', () => {
     describe('When the id is invalid', () => {
+      before(async () => {
+        stub(schema, 'validate').resolves({ error: { name: 'ValidationError' } });
+      });
+
       it('Should throw an error', async () => {
-        
+        try {
+          await productService.validateId({ id: 'a' })
+        } catch (error) {
+          expect(error.name).to.be.equal('ValidationError');
+        }
       });
     });
 
     describe('When the id is valid', () => {
-      it('Should return an object with the id', async () => {
-        
+      before(async () => {
+        stub(schema, 'validate').resolves({ value: { id: 1 } });
+      });      
+
+      it('Should return an object with the id', async () => {        
+        const result = await productService.validateId({ id: '1' });
+        expect(result).to.deep.equal({ id: 1 })
+        expect(result).not.to.deep.equal({ id: '1' });
       });
 
     });
   })
 
-  describe('Validating a product format update/create resquest', () => {
-    describe('When the body is invalid', () => {
-      it('Should throw an error', () => {
+  // describe('Validating a product format update/create resquest', () => {
+  //   describe('When the body is invalid', () => {
+  //     it('Should throw an error', () => {
 
-      })
-    });
-    describe('When the format is valid', () => { 
-      it('Should return an object with the product data', () => {
+  //     })
+  //   });
+  //   describe('When the format is valid', () => { 
+  //     it('Should return an object with the product data', () => {
 
-      });
-    });
-  });
+  //     });
+  //   });
+  // });
 });
