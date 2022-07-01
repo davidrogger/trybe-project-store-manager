bbc77b6b3ecb60a1e02b6960871087e78739ddf5
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-01 18:35:50 -0300

    feat: Rote para sale getById
    implementada rota para requisição de venda por id

--- routers/saleRouter.js
+++ routers/saleRouter.js
@@ -4,6 +4,7 @@ const rescue = require('express-rescue');
 const saleRouter = Router();
 const saleController = require('../controllers/saleController');
 
+saleRouter.get('/:id', rescue(saleController.getById));
 saleRouter.get('/', rescue(saleController.getAll));
 
 module.exports = saleRouter;
