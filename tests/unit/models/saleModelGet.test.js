9de8bc20cfd8ade588144638cb1aab1bc7449409
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-01 15:16:46 -0300

    test: saleModel getAll
    Adicionando stub para o teste ficar isolado

--- tests/unit/models/saleModelGet.test.js
+++ tests/unit/models/saleModelGet.test.js
@@ -1,13 +1,22 @@
 
 const { expect } = require('chai');
+const { stub } = require('sinon');
+const connection = require('../../../models/connection');
 const saleProduct = require('../../../models/saleModel');
 
 describe('Testing saleModel GET', () => {
   describe.only('Getting all sales', () => {
+    before(async () => {
+      stub(connection, 'query').resolves([[{}]])
+    });
+    after(() => {
+      connection.query.restore();
+    })
     it('Should return an array of objects', async () => {
       const result = await saleProduct.getAll();
       expect(result).to.be.an('array');
-      expect(result).to.be.empty;
+      expect(result[0]).to.be.an('object');
+      expect(result).not.to.be.empty;
     });
   });
   // describe('Getting sales by Id', () => { });
