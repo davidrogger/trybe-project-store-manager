5d9217b4dcdb928f10fa7a14b98a84eead7f0e02
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-04 15:00:54 -0300

    feat: saleRouter remove
    Implementada rota para remover venda por id

--- routers/saleRouter.js
+++ routers/saleRouter.js
@@ -5,6 +5,7 @@ const saleRouter = Router();
 const saleController = require('../controllers/saleController');
 
 saleRouter.get('/:id', rescue(saleController.getById));
+saleRouter.delete('/:id', rescue(saleController.remove));
 saleRouter.get('/', rescue(saleController.getAll));
 saleRouter.post('/', rescue(saleController.add));
 
