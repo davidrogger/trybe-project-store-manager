b3ccade63510d91ea4b6d3a542bd58bd315b0474
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-04 09:20:39 -0300

    test: saleService add
    Adicionado teste do service que registra a venda e os produtos

---
+++ tests/unit/services/saleServiceAdd.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const { stub } = require('sinon');
+const saleModel = require('../../../models/saleModel');
+const saleService = require('../../../services/saleService');
+
+const { rightSaleBody } = require('../../../__tests__/_dataMock');
+
+describe('Testing saleService ADD', () => {
+  before(async () => {
+    stub(saleModel, 'addSale').resolves({ id: 4 });
+    stub(saleModel, 'soldProduct').resolves(rightSaleBody);
+  });
+  after(() => {
+    saleModel.addSale.restore();
+    saleModel.soldProduct.restore();
+  });
+
+  describe('When a sale is requested', () => {
+    it('Should return an object with the sale id and the list of product sold', async () => {
+      const result = await saleService.add(rightSaleBody);
+      expect(result.id).to.be.equal(4);
+      expect(result.itemSold).to.deep.equal(rightSaleBody);
+    });
+  });
+});
