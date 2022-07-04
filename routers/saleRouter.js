2e0488bf432b55fd066ce758b48d0470ca85622d
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-04 13:45:49 -0300

    feat: saleRouter add
    Implementada rota para cadastro de venda

--- routers/saleRouter.js
+++ routers/saleRouter.js
@@ -6,5 +6,6 @@ const saleController = require('../controllers/saleController');
 
 saleRouter.get('/:id', rescue(saleController.getById));
 saleRouter.get('/', rescue(saleController.getAll));
+saleRouter.post('/', rescue(saleController.add));
 
 module.exports = saleRouter;
