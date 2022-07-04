a400578cbdba3c2ef630b11389964255109b23c7
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-04 16:11:54 -0300

    test: saleModel remove
    teste para remover produtos da venda

--- tests/unit/models/saleModelRemove.test.js
+++ tests/unit/models/saleModelRemove.test.js
@@ -4,10 +4,10 @@ const connection = require('../../../models/connection');
 const saleModel = require('../../../models/saleModel');
 
 describe('Testing saleModel REMOVE', () => {
-  before(async () => {
+  beforeEach(async () => {
     stub(connection, 'query').resolves();
   })
-  after(() => {
+  afterEach(() => {
     connection.query.restore();
   });
   describe('Removing a sale', () => {
@@ -15,5 +15,11 @@ describe('Testing saleModel REMOVE', () => {
       await saleModel.remove({});
       expect(connection.query.called).to.be.equal(true);
     })
-  })
+  });
+  describe('Removing products from a sale', () => {
+    it('Should call connection with the database to remove the products from the data', async () => {
+      await saleModel.removeProducts({});
+      expect(connection.query.called).to.be.equal(true);
+    });
+  });
 });
