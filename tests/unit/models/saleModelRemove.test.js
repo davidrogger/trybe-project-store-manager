f8ff0eb01629570ce33cda6c2d9fab4747414054
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-04 14:36:45 -0300

    test: saleController remove
    adicionado before after para analise do avaliador falhando remotamente

--- tests/unit/models/saleModelRemove.test.js
+++ tests/unit/models/saleModelRemove.test.js
@@ -4,12 +4,14 @@ const connection = require('../../../models/connection');
 const saleModel = require('../../../models/saleModel');
 
 describe('Testing saleModel REMOVE', () => {
-  afterEach(() => {
+  before(async () => {
+    stub(connection, 'query').resolves();
+  })
+  after(() => {
     connection.query.restore();
   });
   describe('Removing a sale', () => {
     it('Should call connection with the database to remove the data', async () => {
-      stub(connection, 'query').resolves();
       await saleModel.remove({});
       expect(connection.query.called).to.be.equal(true);
     })
