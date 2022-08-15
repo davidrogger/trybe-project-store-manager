f5bb56e3904e1ddde85b382943d86460e3cb91a3
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-15 17:17:04 -0300

    feat: swagger configuration updated
    add division between endpoints in the documentation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
+
 const app = express();
 const productRouter = require('./routers/productRouter');
 const saleRouter = require('./routers/saleRouter');
