0ad74d28356f1f3470991983b00ab9e2a98af3fc
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-16 09:28:03 -0300

    feat: swagger generator test

--- swagger.js
+++ swagger.js
@@ -1,6 +1,25 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+const doc = {
+  info: {
+    version: '1.0.0',
+    title: 'Project Storage Manager',
+    description: 'Meu primeiro CRUD API aplicando arquitetura MSC',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'Products',
+      description: 'Endpoints',
+    },
+  ],
+};
+
 const outputFile = './swagger_output.json';
 const endpointsFiles = ['./app.js'];
 
-swaggerAutogen(outputFile, endpointsFiles);
+swaggerAutogen(outputFile, endpointsFiles, doc);
