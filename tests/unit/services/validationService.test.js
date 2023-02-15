a1082c1b5884c3ac5fb8a42c7bf5db549d788aa5
davidrogger <davidrogger.a@gmail.com>
2023-02-15 16:18:27 -0300

    TEST: id validation

--- tests/unit/services/validationService.test.js
+++ tests/unit/services/validationService.test.js
@@ -1,23 +1,28 @@
 const { validate } = require('../../../services/validationService');
 
 const { expect } = require('chai');
-const { stub } = require('sinon');
-const Joi = require('joi');
 
 describe('Testing validationService', () => {
   describe('Validating an "id"', () => {
     describe("When the id is valid", () => {
-      it("Should return the id type number", () => {
-        const validIDs = ["1", "2", "3", "4", "5"];
-        validIDs.map(async (validId) => {
-          const { id } = await validate.id({ id: validId })
+      it("Should return the id type number", async () => {
+        const { id } = await validate.id({ id: "1" })
           expect(typeof(id)).to.be.equal('number');
           expect(typeof(id)).not.to.be.equal('string');
-          expect(id).to.be.equal(Number(validId));
-        })
+          expect(id).to.be.equal(1);
       })
     });
-    describe("When the id is invalid", () => {});
+    describe("When the id is invalid", () => {
+      it('Should throw an error', async () => {
+          try {
+            await validate.id({ id: 'a'})
+          } catch (error) {
+            expect(error.name).to.be.equal("ValidationError");
+            expect(error.message).to.be.equal('"id" must be a number');
+          }
+
+      })
+    });
   });
   describe('Validating an "productBody"', () => {
     describe("When the body is valid", () => {});
