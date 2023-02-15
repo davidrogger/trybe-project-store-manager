761ce3c26c1f2c2fb9f50c65ff4ee8bde1b0869e
davidrogger <davidrogger.a@gmail.com>
2023-02-15 17:03:11 -0300

    TEST: product sales validation

--- tests/unit/services/validationService.test.js
+++ tests/unit/services/validationService.test.js
@@ -1,6 +1,7 @@
 const { validate } = require('../../../services/validationService');
 
 const { expect } = require('chai');
+const { valid } = require('joi');
 
 describe('Testing validationService', () => {
   describe('Validating an "id"', () => {
@@ -37,7 +38,61 @@ describe('Testing validationService', () => {
   });
 
   describe('Validating a "productSalesBody"', () => {
-    it("Should return the sales body when the body is valid", async () => {});
-    it("Should throw an error when the body is invalid", async () => {});
+    it("Should return the sales body when the body is valid", async () => {
+      const sales = [
+        {
+          productId: 2,
+          quantity: 5
+        }
+      ];
+      const { sales: validateSales } = await validate.productSalesBody({ sales });
+      expect(validateSales).to.be.deep.equal(sales);
+    });
+    describe("When the body is invalid", () => {
+      it("Should throw an error when missing 'productId' or 'quantity'", async () => {
+        const missingFields = [
+          {
+            missing: "productId",
+            sales: [{ quantity: 2 }]
+          },
+          {
+            missing: "quantity",
+            sales: [{ productId: 2 }]
+          }
+        ];
+
+        for (const product of missingFields) {
+          const { missing, sales } = product;
+          try {
+            await validate.productSalesBody({ sales });
+          } catch (error) {
+            expect(error.name).to.be.equal('ValidationError');
+            expect(error.message).to.be.equal(`"${missing}" is required`);
+          } 
+        }
+      })
+      it("Should throw an error when 'quantity' and 'productId' values are not number", async () => {
+        const missingFields = [
+          {
+            field: "productId",
+            sales: [{ productId: 'invalid', quantity: 2 }]
+          },
+          {
+            field: "quantity",
+            sales: [{ productId: 2, quantity: 'invalid' }]
+          }
+        ];
+
+        for (const product of missingFields) {
+          const { field, sales } = product;
+          try {
+            await validate.productSalesBody({ sales });
+          } catch (error) {
+            expect(error.name).to.be.equal('ValidationError');
+            expect(error.message).to.be.equal(`"${field}" must be a number`);
+          } 
+        }
+      })
+    });
   });
 });
